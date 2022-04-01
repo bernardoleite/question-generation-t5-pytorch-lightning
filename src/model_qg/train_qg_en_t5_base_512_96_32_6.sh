cbde78c75c4e073393c3f0fb4c535f8c57e7a93a
bernardoleite <up201404464@gcloud.fe.up.pt>
2022-04-01 22:23:11 +0100

    Update train_qg_en_t5_base_512_96_32_6.sh

--- src/model_qg/train_qg_en_t5_base_512_96_32_6.sh
+++ src/model_qg/train_qg_en_t5_base_512_96_32_6.sh
@@ -2,7 +2,7 @@
 
 for ((i=42; i <= 42; i++))
 do
-	python train.py \
+	taskset --cpu-list 1-22 python train.py \
 	 --dir_model_name "qg_en_t5_base_512_96_32_6_seed_${i}" \
 	 --model_name "t5-base" \
 	 --tokenizer_name "t5-base" \
