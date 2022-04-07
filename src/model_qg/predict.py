8e02e5039fba95a8287eb11a0e34b074a786caa2
bernardoleite <up201404464@gcloud.fe.up.pt>
2022-04-07 22:44:18 +0100

    Update predict.py

--- src/model_qg/predict.py
+++ src/model_qg/predict.py
@@ -1,37 +1,123 @@
-    
-import torch
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-trained_model_path = '../../model/'
-trained_tokenizer = '../../tokenizer/'
-
-model = T5ForConditionalGeneration.from_pretrained(trained_model_path)
-tokenizer = T5Tokenizer.from_pretrained(trained_tokenizer)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print ("device ",device)
-model = model.to(device)
-
-context ="Mary and John are a couple."
-answer = "John"
-text = "context: "+ context + " " + "answer: " + answer + " </s>"
-print (text)
-
-encoding = tokenizer("Mary", "Mary and John are a couple.", max_length = 64, padding=True, return_tensors="pt")
-print (encoding.keys())
-input_ids = encoding["input_ids"].to(device)
-attention_mask = encoding["attention_mask"].to(device)
-
-model.eval()
-beam_outputs = model.generate(
-    input_ids=input_ids,
-    attention_mask=attention_mask,
-    max_length=96,
-    early_stopping=True,
-    num_beams=5,
-    num_return_sequences=3
+from transformers import (
+    T5ForConditionalGeneration,
+    MT5ForConditionalGeneration,
+    T5Tokenizer
 )
 
-for beam_output in beam_outputs:
-    sent = tokenizer.decode(beam_output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-    print(sent)
+import argparse
+import pandas as pd
+import sys
+sys.path.append('../')
+
+from models import T5FineTuner
+from utils import currentdate
+import time
+import os
+import json
+import torch
+
+ANSWER = "Hans"
+CONTEXT ="""
+A vida de Hans mais uma vez tinha virado. Já não eram as longas navegações até aos confins dos continentes, o avançar aventuroso ao longo das costas luxuriantes e de costas desérticas, de povo em povo, de baía em baía. Agora verificava a ordem dos armazéns, o bom estado dos navios, a competência das equipagens, controlava as cargas e descargas, discutia negócios e contratos. As suas viagens tornaram-se rápidas e espaçadas.
+"""
+
+def generate(args, device, qgmodel: T5FineTuner, tokenizer: T5Tokenizer,  answer: str, context: str) -> str:
+
+    source_encoding = tokenizer(
+        answer,
+        context,
+        max_length=args.max_len_input,
+        padding='max_length',
+        truncation = 'only_second',
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors='pt'
+    )
+
+    # Put this in GPU (faster than using cpu)
+    input_ids = source_encoding['input_ids'].to(device)
+    attention_mask = source_encoding['attention_mask'].to(device)
+
+    generated_ids = qgmodel.model.generate(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        num_return_sequences=args.num_return_sequences, # defaults to 1
+        num_beams=args.num_beams, # defaults to 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! myabe experiment with 5
+        max_length=args.max_len_output,
+        repetition_penalty=args.repetition_penalty, # defaults to 1.0, #last value was 2.5
+        length_penalty=args.length_penalty, # defaults to 1.0
+        early_stopping=True, # defaults to False
+        use_cache=True
+    )
+
+    preds = {
+        tokenizer.decode(generated_id, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        for generated_id in generated_ids
+    }
+    
+    return ''.join(preds)
+
+
+def run(args):
+    # Load args (needed for model init) and log json
+    params_dict = dict(
+        checkpoint_model_path = args.checkpoint_model_path,
+        model_name = args.model_name,
+        tokenizer_name = args.tokenizer_name,
+        max_len_input = args.max_len_input,
+        max_len_output = args.max_len_output,
+        num_beams = args.num_beams,
+        num_return_sequences = args.num_return_sequences,
+        repetition_penalty = args.repetition_penalty,
+        length_penalty = args.length_penalty,
+    )
+    params = argparse.Namespace(**params_dict)
+
+    # Load T5 base Tokenizer
+    t5_tokenizer = T5Tokenizer.from_pretrained(args.tokenizer_name)
+    # Load T5 base Model
+    if "mt5" in args.model_name:
+        t5_model = MT5ForConditionalGeneration.from_pretrained(args.model_name)
+    else:
+        t5_model = T5ForConditionalGeneration.from_pretrained(args.model_name)
+    # Load T5 fine-tuned model for QG
+    checkpoint_model_path = args.checkpoint_model_path
+    qgmodel = T5FineTuner.load_from_checkpoint(checkpoint_model_path, hparams=params, t5model=t5_model, t5tokenizer=t5_tokenizer)
+
+    # Put model in freeze() and eval() model. Not sure the purpose of freeze
+    # Not sure if this should be after or before changing device for inference.
+    qgmodel.freeze()
+    qgmodel.eval()
+
+    # Put model in gpu (if possible) or cpu (if not possible) for inference purpose
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    qgmodel = qgmodel.to(device)
+    print ("Device for inference:", device)
+
+    generated = generate(args, device, qgmodel, t5_tokenizer, ANSWER, CONTEXT)
+    print(generated)
+
+
+if __name__ == '__main__':
+    # Initialize the Parser
+    parser = argparse.ArgumentParser(description = 'Generate questions and save them to json file.')
+
+    # Add arguments
+    parser.add_argument('-cmp','--checkpoint_model_path', type=str, metavar='', default="../../checkpoints/qg_en_t5_base_512_96_32_6_seed_42/model-epoch=01-val_loss=1.36.ckpt", required=False, help='Model folder checkpoint path.')
+
+    parser.add_argument('-mn','--model_name', type=str, metavar='', default="t5-base", required=False, help='Model name.')
+    parser.add_argument('-tn','--tokenizer_name', type=str, metavar='', default="t5-base", required=False, help='Tokenizer name.')
+
+    parser.add_argument('-mli','--max_len_input', type=int, metavar='', default=512, required=False, help='Max len input for encoding.')
+    parser.add_argument('-mlo','--max_len_output', type=int, metavar='', default=96, required=False, help='Max len output for encoding.')
+
+    parser.add_argument('-nb','--num_beams', type=int, metavar='', default=5, required=False, help='Number of beams.')
+    parser.add_argument('-nrs','--num_return_sequences', type=int, metavar='', default=1, required=False, help='Number of returned sequences.')
+    parser.add_argument('-rp','--repetition_penalty', type=float, metavar='', default=1.0, required=False, help='Repetition Penalty.')
+    parser.add_argument('-lp','--length_penalty', type=float, metavar='', default=1.0, required=False, help='Length Penalty.')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Start tokenization, encoding and generation
+    run(args)
